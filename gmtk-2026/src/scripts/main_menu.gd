c402d2f0bfff87b0f26a6540c0369a976e92d7d0
ElNegrito188 <aqualung188@gmail.com>
2026-07-25 23:47:31 -0300

    Auto stash before merge of "main" and "origin/main"

--- gmtk-2026/src/scripts/main_menu.gd
+++ gmtk-2026/src/scripts/main_menu.gd
@@ -1,4 +1,10 @@
 extends Control
+@onready var main_menu: Control = $"."
+@onready var audio_player: AudioStreamPlayer3D = $Music
+
+func _ready() -> void:
+	await get_tree().process_frame
+	audio_player.play()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/main.tscn")
